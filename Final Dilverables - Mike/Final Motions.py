cc83d8b9cda6ade87a4ef66211bd0b4aea94b244
Michael Shea <mjshea3@illinois.edu>
2018-04-26 12:44:46 -0500

    finished final part

---
+++ Final Dilverables - Mike/Final Motions.py
@@ -0,0 +1,42 @@
+from functions import *
+
+# starting position
+print('Moving to Initial Position')
+rarray = np.array([0,0,0,0,0,0,0,0])
+larray = np.array([0,0,0,0,0,0,0,0])
+totalLeft(larray)
+totalRight(rarray)
+
+.7
+playHenoi()
+
+# =============================================================================
+# getLeftToPoint()
+# getLeftToPoint()
+# LeftCup(True)
+# getLeftToPoint()
+# getLeftToPoint()
+# LeftCup(False)
+# getRightToPoint()
+# =============================================================================
+
+
+endSim()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
